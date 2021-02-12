d11cd0d74fa2201a105bf41711dc192763a890e3
husenindia <husentelwala@gmail.com>
2021-02-12 15:23:48 +0530

    7. Pipe the readable streams to writable streams

--- 6.readable-streams/app.js
+++ 6.readable-streams/app.js
@@ -1,18 +1,8 @@
 var fs = require('fs');
-var http = require('http');
-
 
 var myReadStreams = fs.createReadStream(__dirname + '/readme.txt','utf8');
 var myWriteStrems = fs.createWriteStream(__dirname + '/writeme.txt');
+
 myReadStreams.on('data',function(chunk){
     myWriteStrems.write(chunk);
 })
-
-// var server = http.createServer(function(req,res){
-//     res.writeHead('200',{'Content-Type':'text/plain'});
-//     res.end('Server created successfully');
-// })
-
-
-// server.listen(3000,'127.0.0.1');
-// console.log('Listening to server 127.0.0.1 Port Number 3000'); 
