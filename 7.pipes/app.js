9f2f742f18b6a387051803fd642539642c595d6b
husenindia <husentelwala@gmail.com>
2021-02-12 15:30:51 +0530

    8.Streams to server via pipes

--- 7.pipes/app.js
+++ 7.pipes/app.js
@@ -1,19 +1,6 @@
 var fs = require('fs');
-var http = require('http');
-
 
 var myReadStreams = fs.createReadStream(__dirname + '/readme.txt','utf8');
 var myWriteStrems = fs.createWriteStream(__dirname + '/writeme.txt');
 
-
 myReadStreams.pipe(myWriteStrems);
-
-
-// var server = http.createServer(function(req,res){
-//     res.writeHead('200',{'Content-Type':'text/plain'});
-//     res.end('Server created successfully');
-// })
-
-
-// server.listen(3000,'127.0.0.1');
-// console.log('Listening to server 127.0.0.1 Port Number 3000'); 
---
+++ 8.streams-to-server/app.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var http = require('http');
+
+
+
+
+var server = http.createServer(function(req,res){
+    res.writeHead('200',{'Content-Type':'text/plain'});
+    var myReadStreams = fs.createReadStream(__dirname + '/readme.txt','utf8');
+    myReadStreams.pipe(res);
+})
+
+
+server.listen(3000,'127.0.0.1');
+console.log('Listening to server 127.0.0.1 Port Number 3000'); 
